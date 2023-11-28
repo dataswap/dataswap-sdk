f830aa5271eff1b32aec4e60a6deb228809a45f0
LoveL <lovel888@hotmail.com>
2023-11-28 09:16:31 +0800

    feat: Add mocha test code

---
+++ test/test.test.ts
@@ -0,0 +1,23 @@
+import { DatasetMetadataEvm } from "../dist/dataset/metadata/repo/evm";
+import { describe } from "mocha";
+import { expect } from "chai";
+import DatasetsDeployment from "../deployments/localnet/Datasets.json"
+
+// describe("RankingHolder", () => {
+//     describe("getDatasetMetadata",async ()=>{
+//     expect(55).to.equal(55);
+//     await console.log("meta title:")
+//     });
+// });
+
+describe("datasetMetadata",()=>{
+    it("getDatasetMetadata",async ()=>{
+            let metadata = new DatasetMetadataEvm(
+                DatasetsDeployment.abi,
+                DatasetsDeployment.address,
+                "http://14.198.182.245:1234/rpc/v1"
+            )
+            let meta = await metadata.getDatasetMetadata(1)
+            console.log("meta title:",meta.data)
+    });
+})
