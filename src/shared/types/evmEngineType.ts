e6f10706de3d81dfadbc45ca3674796c533c381a
LoveL <lovel888@hotmail.com>
2023-12-20 11:02:27 +0800

    feat: Add EvmEx class to include constructor EvmEngine

--- src/shared/types/evmEngineType.ts
+++ src/shared/types/evmEngineType.ts
@@ -3,7 +3,16 @@ import {
     Web3EvmEngine,
     EthersWallet,
     Web3Wallet,
+    Evm
 } from "@unipackage/net"
+import { AbiFunctionFragment } from "web3"
 
 export const EvmEngine = EthersEvmEngine
 export const Wallet = EthersWallet
+
+
+export class EvmEx extends Evm {
+    constructor(abi: AbiFunctionFragment[], address: string, url: string) {
+        super(new EvmEngine(abi, address, url))
+    }
+}
