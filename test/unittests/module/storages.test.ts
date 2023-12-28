6837cad2b54e1d104dee4f5173f661e54dc15194
LoveL <lovel888@hotmail.com>
2023-12-28 09:44:00 +0800

    test: 💍 Add storages test code
    ✅ Closes: #7

---
+++ test/unittests/module/storages.test.ts
@@ -0,0 +1,59 @@
+/*******************************************************************************
+ *   (c) 2023 dataswap
+ *
+ *  Licensed under either the MIT License (the "MIT License") or the Apache License, Version 2.0
+ *  (the "Apache License"). You may not use this file except in compliance with one of these
+ *  licenses. You may obtain a copy of the MIT License at
+ *
+ *      https://opensource.org/licenses/MIT
+ *
+ *  Or the Apache License, Version 2.0 at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the MIT License or the Apache License for the specific language governing permissions and
+ *  limitations under the respective licenses.
+ ********************************************************************************/
+
+import { describe } from "mocha"
+import { getContractsManager, getGenerator } from "../../fixtures"
+import { DatasetsHelper } from "../../helpers/module/datasetsHelper"
+import { StoragesAssertion } from "../../assertions/module/storagesAssertion"
+
+/**
+ * Test suite for the Storages functionality.
+ */
+describe("storages", () => {
+    let storagesAssertion: StoragesAssertion
+    let datasetHelperInstance: DatasetsHelper
+
+    /**
+     * Setup before running the test suite.
+     */
+    before(async function () {
+        this.sharedData = {}
+        this.sharedData.generator = getGenerator()
+        this.sharedData.contractsManager = getContractsManager()
+        let storages = this.sharedData.contractsManager.StoragesEvm()
+        storagesAssertion = new StoragesAssertion(storages)
+
+        datasetHelperInstance = new DatasetsHelper(
+            this.sharedData.generator,
+            this.sharedData.contractsManager
+        )
+        /**
+         * Keep the following code for unit testing
+         */
+        // await datasetHelperInstance.proofSubmittedDatasetWorkflow(true)
+    })
+
+    /**
+     * Test case for submitStorageClaimIds functionality. TODO: Complete storages unit test #100
+     */
+    it.skip("submitStorageClaimIds", async function () {
+        await storagesAssertion.submitStorageClaimIdsAssertion("", 1, 1, [], [])
+    })
+})
