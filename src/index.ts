c0f812f519ce411694ee77c30978c156e09f96b8
waynewyang <yangwei.hit@gmail.com>
2023-12-28 20:19:40 +0800

    feat: 🎸 export feature for debug

--- src/index.ts
+++ src/index.ts
@@ -18,47 +18,37 @@
  *  limitations under the respective licenses.
  ********************************************************************************/
 
-//New type
-// export type { DatasetInfo } from "./dataset/types"
+export { Car, CarReplica } from "./core/carstore/types"
+export { Fund } from "./core/escrow/types"
+export { DataswapMessage } from "./message/types"
+export { DatasetMetadata } from "./module/dataset/metadata/types"
+export { DatasetRequirement } from "./module/dataset/requirement/types"
+export { DatasetChallenge } from "./module/dataset/challenge/types"
+export { MatchingMetadata } from "./module/matching/metadata/types"
+export { MatchingTarget } from "./module/matching/target/types"
+export { MatchingBids } from "./module/matching/bids/types"
+export { CarReplicaState } from "./shared/types/carstoreType"
+export { DatasetRequirements, DatasetState } from "./shared/types/datasetType"
+export { DataType } from "./shared/types/dataType"
+export { EscrowOperateType, EscrowType } from "./shared/types/escrowType"
+export { EvmEngine, EvmEx, Wallet } from "./shared/types/evmEngineType"
+export { DealState } from "./shared/types/filecoinType"
+export { MatchingState } from "./shared/types/matchingType"
 
-// export type {
-//     DatasetCreateInfo,
-//     DatasetMetaInfo,
-//     DatasetOwnerInfo,
-// } from "./base/metadata/types"
+export { CarstoreEvm } from "./core/carstore/repo/evm"
+export { EscrowEvm } from "./core/escrow/repo/evm"
+export { FilecoinEvm } from "./core/filecoin/repo/evm"
+export { FilplusEvm } from "./core/filplus/repo/evm"
+export { RolesEvm } from "./core/roles/repo/evm"
+export { DatasetMetadataEvm } from "./module/dataset/metadata/repo/evm"
+export { DatasetRequirementEvm } from "./module/dataset/requirement/repo/evm"
+export { DatasetProofEvm } from "./module/dataset/proof/repo/evm"
+export { DatasetChallengeEvm } from "./module/dataset/challenge/repo/evm"
+export { MatchingMetadataEvm } from "./module/matching/metadata/repo/evm"
+export { MatchingTargetEvm } from "./module/matching/target/repo/evm"
+export { MatchingBidsEvm } from "./module/matching/bids/repo/evm"
+export { StoragesEvm } from "./module/storages/repo/evm"
+export { DatacapsEvm } from "./module/datacaps/repo/evm"
 
-// export type {
-//     DatasetProofCreateInfo,
-//     DatasetProofInfo,
-//     Leaves,
-// } from "./base/proof/types"
-
-// export type {
-//     DatasetChallengeCreateInfo,
-//     DatasetChallengesInfo,
-//     DatasetChallenge,
-//     DatasetChallengesMap,
-// } from "./dataset/challenge"
-
-// export type {
-//     DatasetDisputeCreateInfo,
-//     DatasetDipputesInfo,
-//     DatasetDispute,
-//     DatasetDisputesMap,
-// } from "./base/dispute/types"
-
-// export type {
-//     DatasetReplicasInfo,
-//     DatasetReplica,
-//     DatasetReplicasMap,
-// } from "./dataset/replicas"
-
-// export type { MatchingInfo } from "./matching"
-
-// export type { MatchingCreateInfo, MatchingMetaInfo } from "./matching/meta"
-
-// export type {
-//     MatchingBid,
-//     MatchingBidCreateInfo,
-//     MatchingBidsMap,
-// } from "./matching/bid"
+export { DataswapMessageMongoDatastore } from "./message/repo/datastore"
+export { DatasetMetadataMongoDatastore } from "./module/dataset/metadata/repo/datastore"
