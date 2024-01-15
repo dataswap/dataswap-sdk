7c1d0f944b15f90cf7de6e8be07421ce547dd601
sirius <sirius.yim@gmail.com>
2024-01-15 16:31:13 +0800

    feat: 🎸 add bidding model
    ✅ Closes: #172

--- src/module/matching/bids/repo/datastore/index.ts
+++ src/module/matching/bids/repo/datastore/index.ts
@@ -17,3 +17,73 @@
  *  See the MIT License or the Apache License for the specific language governing permissions and
  *  limitations under the respective licenses.
  ********************************************************************************/
+
+import { DataStore, DatabaseConnection } from "@unipackage/datastore"
+import { ValueFields } from "@unipackage/utils"
+import { MatchingBid } from "../../types"
+import { MatchingBidDocument, MatchingBidSchema } from "./model"
+import { MongooseDataStore } from "@unipackage/datastore"
+import { MatchingBidsEvm } from "../evm"
+import { newDelegatedEthAddress } from "@glif/filecoin-address"
+/**
+ * Class representing a MongoDB datastore for MatchingBid entities.
+ * Extends the DataStore class with MatchingBid and MatchingBidDocument.
+ * @class
+ */
+export class MatchingBidMongoDatastore extends DataStore<
+    ValueFields<MatchingBid>,
+    MatchingBidDocument
+> {
+    /**
+     * Creates an instance of MatchingBidMongoDatastore.
+     * @param {string} uri - The MongoDB connection URI.
+     * @constructor
+     */
+    constructor(connection: DatabaseConnection) {
+        super(
+            new MongooseDataStore<
+                ValueFields<MatchingBid>,
+                MatchingBidDocument
+            >("MatchingBid", MatchingBidSchema, connection)
+        )
+    }
+
+    /**
+     * Asynchronously stores a matching bid with the specified parameters.
+     *
+     * @param options - The options object containing the necessary parameters.
+     *   - `matchingBids`: The Ethereum Virtual Machine instance for matching bids.
+     *   - `matchingBid`: The matching bid to be stored.
+     */
+    async storeMatchingBid(options: {
+        matchingBids: MatchingBidsEvm
+        origionMatchingBid: MatchingBid
+    }) {
+        try {
+            const bids = await options.matchingBids.getMatchingBids(
+                options.origionMatchingBid.matchingId!
+            )
+            if (!bids.ok) {
+                throw bids.error
+            }
+            for (let i = 0; i < bids.data!.bidders.length; i++) {
+                if (
+                    options.origionMatchingBid.bidder ===
+                    newDelegatedEthAddress(bids.data!.bidders[i]).toString()
+                ) {
+                    await this.CreateOrupdateByUniqueIndexes(
+                        new MatchingBid({
+                            bidder: options.origionMatchingBid.bidder,
+                            amount: options.origionMatchingBid.amount,
+                            complyFilplusRule: bids.data!.complyFilplusRules[i],
+                            matchingId: options.origionMatchingBid.matchingId!,
+                        })
+                    )
+                    return
+                }
+            }
+        } catch (error) {
+            throw error
+        }
+    }
+}
